7be9a5a99074e536b7448de48cf033e9a6753d4e
sos171801fsr <fanciscosanchezrivas@gmail.com>
2018-04-07 16:54:47 +0000

    cambios

--- goalsApi/index.js
+++ goalsApi/index.js
@@ -5,19 +5,7 @@ module.exports = goalsApi;
 
 goalsApi.register = function (app,db2) {
     console.log("Register routes for goals API");
-    
-    var insertar = function(elementos, array, limit, offset) {
-        var i = offset;
-        var j = limit;
-        while (j > 0) {
-            array.push(elementos[i]);
-            j--;
-            i++;
-        }
-        return elementos;
-    };
-    
-    var initialteams2 = [
+     var initialteams2 = [
         { 
             "city" : "malaga",
             "year" : 2015,
@@ -60,6 +48,91 @@ goalsApi.register = function (app,db2) {
         }
     ];
     
+var buscador = function(base, aux_set, param_city, param_year, param_team, param_rightfoot, param_head, param_penalty) {
+
+        //console.log("Búsqueda con parametros: stadium = " + param_stadium + ", date = " + param_date + ", hit = " + param_hit + ", run = " + param_run + ", error = " + param_error + ".");
+
+        
+
+        if ( param_city != undefined || param_rightfoot != undefined || param_head != undefined || param_penalty != undefined || param_year != undefined) {
+
+            for (var j = 0; j < base.length; j++) {
+
+                var year = parseInt(base[j].year);
+                var city = base[j].city;
+                var team = base[j].team;
+                var rightfoot = base[j].rightfoot;
+                var head = base[j].head;
+                var penalty = base[j].penalty;
+
+                // City
+                if ( param_city != undefined || param_team == undefined || param_rightfoot == undefined || param_head == undefined || param_penalty == undefined || param_year == undefined) {
+
+                    if ( param_city == city) {
+                        aux_set.push(base[j]);
+                    }
+
+                    //Team
+                }
+                if ( param_city == undefined || param_team != undefined || param_rightfoot == undefined || param_head == undefined || param_penalty == undefined || param_year == undefined) {
+
+                    if ( param_team == team) {
+                        aux_set.push(base[j]);
+                    }
+
+                    
+                }
+                  // Rightfoot
+                else if ( param_city == undefined || param_team == undefined || param_rightfoot != undefined || param_head == undefined || param_penalty == undefined || param_year == undefined) {
+
+                    if (param_rightfoot == rightfoot) {
+                        aux_set.push(base[j]);
+                    }
+                }
+                  // Head
+                
+                else if (param_city == undefined || param_team == undefined || param_rightfoot == undefined || param_head != undefined || param_penalty == undefined || param_year == undefined) {
+
+                    if (param_head == head) {
+                        aux_set.push(base[j]);
+                    }
+
+                    //Penalty
+                }
+                else if (param_city == undefined || param_team == undefined || param_rightfoot == undefined || param_head == undefined || param_penalty != undefined || param_year == undefined) {
+
+                    if (param_penalty == penalty) {
+                        aux_set.push(base[j]);
+                    }
+
+                    // Year
+                }
+                else if (param_city == undefined || param_team == undefined || param_rightfoot == undefined || param_head == undefined || param_penalty == undefined || param_year != undefined) {
+
+                    if (param_year == year ) {
+                        aux_set.push(base[j]);
+                    }
+
+                    // Rightfoot, head, penalty
+                }
+                else if (param_city == undefined || param_team == undefined || param_rightfoot != undefined || param_head != undefined || param_penalty != undefined || param_year == undefined) {
+
+                    if (param_rightfoot == rightfoot && param_head == head && param_penalty == penalty) {
+                        aux_set.push(base[j]);
+                    }
+        
+                }
+            }
+
+        }
+
+
+        return aux_set;
+
+    };
+    
+   
+    
     app.get(BASE_API_PATH + "/goals-stats/docs",(req,res) => {
     res.redirect("https://documenter.getpostman.com/view/3935248/RVu1Gqez");
 });
@@ -85,7 +158,7 @@ goalsApi.register = function (app,db2) {
 
 });
 
-app.get(BASE_API_PATH+"/goals-stats",(req,res)=>{
+/*app.get(BASE_API_PATH+"/goals-stats",(req,res)=>{
     //console.log(Date() + " - GET /teams");
     //res.send(initialteams2);
     db2.find({}).toArray((err,teams)=>{
@@ -99,7 +172,7 @@ app.get(BASE_API_PATH+"/goals-stats",(req,res)=>{
             return c;
         }));
 });
-});
+});*/
 
 app.post(BASE_API_PATH+"/goals-stats",(req,res)=>{
     console.log(Date() + " - POST /teams");
@@ -110,45 +183,7 @@ app.post(BASE_API_PATH+"/goals-stats",(req,res)=>{
     } 
    
     
-      /* var url = req.query;
-        var city = url.city;
-        var year = url.year;
-        var goalsrightfoot = url["goals rightfoot"];
-        var goalshead = url["goals head"];
-        var goalspenalty = url["goals penalty"];
-
-        //variables de paginación
-        var limit = parseInt(url.limit);
-        var offset = parseInt(url.offset);
-        var elementos = [];
-
-
-        if (limit > 0 && offset >= 0){
-            console.log("INFO: New GET request to /builders");
-            db2.find({}).skip(offset).limit(limit).toArray((err, teams) => {
-                if (err){
-                    console.error("WARNING: Error getting data from DB");
-                    res.sendStatus(500); //Internal server error
-                }else{
-                    var filtered = teams.filter((param) => {
-                        if ((city == undefined || param.city == city) && (year == undefined || param.year == year) && 
-                        (goalsrightfoot == undefined || param["goals rightfoot"] == goalsrightfoot) 
-                        && (goalshead = undefined || param["goals head"] == goalshead) 
-                        && (goalspenalty == undefined || param["goals penalty"] == goalspenalty)) {
-                            return param;
-                        }
-                    });
-                }
-                if (filtered.length > 0) {
-                    elementos = insertar(filtered, elementos, limit, offset);
-                    res.send(elementos);
-                }else{
-                    console.log("WARNING: Error getting data from DB");
-                    res.sendStatus(404); //Not found
-                }
-            });
-          
-}else{*/
+     
     db2.find({ "city" : newteam.city}).toArray((err,filteredTeams)=>{
     if(err){
         console.error("Error accesing DB");
@@ -163,7 +198,6 @@ app.post(BASE_API_PATH+"/goals-stats",(req,res)=>{
         }
     
     });
-//}
 });
 
 app.put(BASE_API_PATH+"/goals-stats",(req,res)=>{
@@ -262,4 +296,92 @@ app.put(BASE_API_PATH+"/goals-stats/:city/:team",(req,res)=>{
     db2.update({"city" : team.city, "team" : team.team},team);
     res.sendStatus(200);
 });
+//Busqueda
+app.get(BASE_API_PATH + "/goals-stats", function(request, response) {
+                //if (!checkApiKey(request, response)) return;
+
+        console.log("INFO: New GET request to /baseball-stats ");
+
+        /*PRUEBA DE BUSQUEDA */
+        var limit = parseInt(request.query.limit);
+        var offset = parseInt(request.query.offset);
+        var year = parseInt(request.query.year);
+        var city = request.query.city;
+        var team = request.query.team;
+        var rightfoot = request.query.rightfoot;
+        var head = request.query.head;
+        var penalty = request.query.penalty;
+        var aux = [];
+        var aux2 = [];
+        var aux3 = [];
+
+
+        if (limit || offset >= 0) {
+            db2.find({}).skip(offset).limit(limit).toArray(function(err, filteredTeams) {
+                if (err) {
+                    console.error('WARNING: Error getting data from DB');
+                    response.sendStatus(500); // internal server error
+                    return;
+                }
+                else {
+                    if (filteredTeams.length === 0) {
+                        response.sendStatus(404); //No content
+                        return;
+                    }
+                    console.log("INFO: Sending countries:: " + JSON.stringify(filteredTeams, 2, null));
+                    if ( city || year || team || rightfoot || head || penalty) {
+
+                        aux = buscador(filteredTeams, aux, city, year, team , rightfoot, head, penalty);
+                        if (aux.length > 0) {
+                            aux2 = aux.slice(offset, offset + limit);
+                            //console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(aux, 2, null));
+                            //console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(baseballstats, 2, null));
+                            //console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(aux2, 2, null));
+                            response.send(aux2);
+                        }
+                        else {
+
+                            response.send(aux3); // No content 
+                            return;
+                        }
+                    }
+                    else {
+                        response.send(filteredTeams);
+                    }
+                }
+            });
+
+        }
+        else {
+
+            db2.find({}).toArray(function(err, filteredTeams) {
+                if (err) {
+                    console.error('ERROR from database');
+                    response.sendStatus(500); // internal server error
+                }
+                else {
+                    if (filteredTeams.length === 0) {
+
+                        response.send(filteredTeams);
+                        return;
+                    }
+                    //console.log("INFO: Sending baseball-stats: " + JSON.stringify(baseballstats, 2, null));
+                    if ( city || year || team|| rightfoot || head || penalty) {
+                        aux = buscador(filteredTeams, aux, city, year, team, rightfoot, head, penalty);
+                        if (aux.length > 0) {
+                            response.send(aux);
+                        }
+                        else {
+                            response.sendStatus(404); //No content
+                            return;
+                        }
+                    }
+                    else {
+                        response.send(filteredTeams);
+                    }
+                }
+            });
+        }
+
+    });
 };
