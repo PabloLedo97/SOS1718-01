8d978a07730908eaf1c69e903f130c791dfe5ac5
sos171801pla <pabledant@alum.us.es>
2018-05-03 20:45:04 +0000

    prueba terminada

---
+++ public/testTvfees/T01-loadData.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+var path = require("path");
+
+describe('Data is loaded', function(){
+    it('should show some teams', function(){
+        browser
+        .get('https://sos1718-01-pla-sos171801pla.c9users.io/ManagerApp.html#!/tvfees-stats')
+        .then(function(){
+            element.all(by.repeater('team in teams'))
+            .then(function(teams){
+                browser.takeScreenshot()
+                .then(function(png){
+                    var stream = fs.createWriteStream(path.join(process.cwd(),'testTvfees','output','Img01.png'));
+                    stream.write(new Buffer(png,'base64'));
+                    stream.end();
+                });
+                
+                expect(teams.length).toBeGreaterThan(0);
+            });
+        });
+    });
+});
---
+++ public/testTvfees/T02-addTvfees.js
@@ -0,0 +1,35 @@
+var fs = require("fs");
+var path = require("path");
+
+describe('Add team', function(){
+    it('should add a new team', function(){
+        browser
+        .get('https://sos1718-01-pla-sos171801pla.c9users.io/ManagerApp.html#!/tvfees-stats')
+        .then(function(){
+            element.all(by.repeater('team in teams'))
+            .then(function(initialteams){
+                browser.driver.sleep(2000);
+                
+                element(by.model('newteam.city')).sendKeys('sevilla');
+                element(by.model('newteam.year')).sendKeys('2015');
+                element(by.model('newteam.team')).sendKeys('real-betis');
+                element(by.model('newteam.capacity')).sendKeys('60000');
+                element(by.model('newteam.attotal')).sendKeys('120000');
+                element(by.model('newteam.ataverage')).sendKeys('550000');
+                
+                element(by.buttonText('Add')).click().then(function(){
+                    element.all(by.repeater('team in teams')).then(function (teams){
+                        expect(teams.length).toEqual(initialteams.length+1);
+                    });
+                });
+                browser.takeScreenshot()
+                .then(function(png){
+                    var stream = fs.createWriteStream(path.join(process.cwd(),'testTvfees','output','Img02.png'));
+                    stream.write(new Buffer(png,'base64'));
+                    stream.end();
+                });
+        
+            });
+        });
+    });
+});
