4e8c28314604958f7836d005a949cc0b84a51f88
sos171801fsr <fanciscosanchezrivas@gmail.com>
2018-05-05 19:46:03 +0000

    cambios public

--- public/goalsmain-ctrl.js
+++ public/goalsmain-ctrl.js
@@ -57,11 +57,11 @@ Highcharts.chart('analytics1', {
   
  });
   
-  }]);   
-    /*Geochart*/
-    /*
-    $http.get("api/v1/tvfees-stats").then(function(response){
-              google.charts.load('current', {
+   
+       /*Geocharts*/
+ $http.get("/api/v1/goals-stats").then(function(response) {
+            console.log((response.data));
+                  google.charts.load('current', {
         'packages':['geochart'],
         // Note: you will need to get a mapsApiKey for your project.
         // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
@@ -72,54 +72,32 @@ Highcharts.chart('analytics1', {
       function drawRegionsMap() {
         var data = google.visualization.arrayToDataTable([
           ['City','PassRate'],
-          ['Barcelona',parseInt(response.data.filter(d=>d.city=='barcelona')
-          .map(function(d){ return (parseInt(d["year"])+parseInt(d["capacity"])+parseInt(d["attotal"])+parseInt(d["ataverage"]))
-          }))],
-          ['Bilbao',parseInt(response.data.filter(d=>d.city=='bilbao')
-          .map(function(d){ return (parseInt(d["year"])+parseInt(d["capacity"])+parseInt(d["attotal"])+parseInt(d["ataverage"]))
-          }))],
-           ['Madrid',parseInt(response.data.filter(d=>d.city=='madrid')
-          .map(function(d){ return (parseInt(d["year"])+parseInt(d["capacity"])+parseInt(d["attotal"])+parseInt(d["ataverage"]))
-          }))],
-          ['Madrid',parseInt(response.data.filter(d=>d.city=='madrid')
-          .map(function(d){ return (parseInt(d["year"])+parseInt(d["capacity"])+parseInt(d["attotal"])+parseInt(d["ataverage"]))
-          }))],
-          ['Valencia',parseInt(response.data.filter(d=>d.city=='valencia')
-          .map(function(d){ return (parseInt(d["year"])+parseInt(d["capacity"])+parseInt(d["attotal"])+parseInt(d["ataverage"]))
-          }))],
-          ['Sevilla',parseInt(response.data.filter(d=>d.city=='sevilla')
-          .map(function(d){ return (parseInt(d["year"])+parseInt(d["capacity"])+parseInt(d["attotal"])+parseInt(d["ataverage"]))
-          }))],
+          ['Malaga','malaga-fc'],
+          ['Sevilla','sevilla-fc'],
+          ['Sevilla2','real-betis-balompie'],
+          ['Bilbao','athletic-club-bilbao'],
+          ['Villareal','villareal-cf'],
+          ['Madrid','realmadrid-fc']
         ]);
 
         var options = {
             region: 'ES',
-             displayMode: 'markers',
-             colorAxis: {colors: ['#00853f', 'green', '#e31b23']},
+            colorAxis: {colors: ['#00853f', 'green', '#e31b23']},
+        displayMode: 'markers',
+        colorAxis: {colors: ['red','grey','black', 'orange']}
         };
 
-        var chart = new google.visualization.GeoChart(document.getElementById('analytics'));
+        var chart = new google.visualization.GeoChart(document.getElementById('goals'));
 
         chart.draw(data, options);
       }
-      
-      
-      
-     
-      
 
+ });   
 
+        $http.get("/api/v1/goals-stats").then(function(response) {
+            console.log((response.data));
 
+            /*Graphosurus*/
 
-      
-      
-      
-      
- 
-      
-    });
-    
-   */ 
-
-
-  
+        });
+  }]); 
