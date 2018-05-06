5c7cefc3487845176ea80c068e552efaf3825fe3
sos171801pla <pabledant@alum.us.es>
2018-05-06 21:18:23 +0000

    finiquitao

--- SOS1718-01/public/testTransfers/config.js
+++
@@ -1,7 +0,0 @@
-exports.config = {
-    seleniumAddress: "http://localhost:8910",
-    specs: ['t01-loadDataTransfers.js','t02-addTeamTransfers.js'],
-    capabilities:{
-        'browserName': 'phantomjs'
-    }
-}
