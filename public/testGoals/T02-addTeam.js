4b440ed0c6947928979d7c508db4d08e669ceade
sos171801fsr <fanciscosanchezrivas@gmail.com>
2018-05-23 16:33:18 +0000

    automatizacion pruebas front-end

--- public/testGoals/T02-addTeam.js
+++ public/testGoals/T02-addTeam.js
@@ -24,7 +24,7 @@ describe('Add team', function(){
                 });
                 browser.takeScreenshot()
                 .then(function(png){
-                    var stream = fs.createWriteStream(path.join(process.cwd(),'testGoals','output','T02.png'));
+                    var stream = fs.createWriteStream(path.join(process.cwd(),'public/testGoals','output','T02.png'));
                     stream.write(new Buffer(png,'base64'));
                     stream.end();
                 });
