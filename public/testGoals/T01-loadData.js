d646d2aa1a218c678b78bb68017778ce88304568
sos171801fsr <fanciscosanchezrivas@gmail.com>
2018-05-24 16:15:13 +0000

    automatizancion de pruebas front-end

--- public/testGoals/T01-loadData.js
+++ public/testGoals/T01-loadData.js
@@ -1,10 +1,11 @@
 var fs = require("fs");
 var path = require("path");
+var config = require("./config");
 
 describe('Data is loaded', function(){
     it('should show some teams', function(){
         browser
-        .get('https://sos1718-01.herokuapp.com/ManagerApp.html#!/goals-stats')
+        .get(config.getAppUrl())
         .then(function(){
             element.all(by.repeater('team in teams'))
             .then(function(teams){
