f81481cf08a14ef5d0efa9bc8cb1d3b29109c5aa
sos171801fsr <fanciscosanchezrivas@gmail.com>
2018-05-26 14:55:10 +0000

    cambios

--- public/testGoals/T00-ApiGoals.js
+++ public/testGoals/T00-ApiGoals.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 describe('API should works', function() {
 	newman.run({
-		collection:require(path.join(process.cwd(),"public/testGoals","sos1718-01-goals-stats.postman_collection.json")),
+		collection:require(path.join(process.cwd(),"public/testGoals","sos1718-01-goals-statsCopy.postman_collection.json")),
 		reporters: "cli"
 	},function(err){
 		if(err)
